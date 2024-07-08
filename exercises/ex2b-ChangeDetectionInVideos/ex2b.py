462291aad10eec2dcef5826101e1a92d63653903
Mads Richardt <mads.richardt@gmail.com>
2024-07-08 13:16:20 +0200

    mads

--- exercises/ex2b-ChangeDetectionInVideos/ex2b.py
+++ exercises/ex2b-ChangeDetectionInVideos/ex2b.py
@@ -1,12 +1,17 @@
+from re import T
+from weakref import ref
 import cv2
 import numpy as np
 import time
 
 # Open a video capture (0 for the default camera)
-camPath = '/home/madsr2d2/VID_20240206_161421.mp4'
+camPath = '/home/madsr2d2/test.mp4'
 cap = cv2.VideoCapture(camPath, cv2.CAP_FFMPEG)
+refFrame = None
+T = 50
 
 while True:
+
     start = time.time()
     # Read a frame from the video capture
     ret, frame = cap.read()
@@ -14,12 +19,25 @@ while True:
         print('Error reading frame')
         break
 
+    # Get the reference frame and convert it to grayscale and float
+    if refFrame is None:
+        refFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        refFrame = refFrame.astype(np.float32)
+        continue
+
     grayImage = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     # Convert the image to float
     grayImgAsFloat = grayImage.astype(np.float32)
 
-    # conert image to uint8
+    # Calculate the absolute difference between the current frame and the reference frame
+    diff = np.abs(grayImgAsFloat - refFrame)
+
+    # Generate binary image based on the threshold T
+    _, mask = cv2.threshold(diff, T, 255, cv2.THRESH_BINARY)
+    print(mask)
+
+    # covert image to uint8
     grayImageUint8 = grayImgAsFloat.astype(np.uint8)
 
     stop = time.time()
@@ -29,6 +47,7 @@ while True:
 
     # Display the frame
     cv2.imshow('Frame', grayImageUint8)
+    cv2.imshow('Mask', mask)
 
     # Wait for 1 millisecond for a key event
     if cv2.waitKey(1) & 0xFF == ord('q'):
